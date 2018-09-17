eee9a9c33fa672292e73616809c97d9fd94464f3
Robin Chou <robin.m.chou@gmail.com>
2018-09-18 00:30:59 +0800

    Initial Commit

---
+++ Triangle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Triangle
+//
+//  Created by robin on 2018/9/18.
+//  Copyright © 2018年 qomo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
